99407f25b5cd5c15a7b7278e1ff538fa7a2e6374
Agampodige <senishkarashith@gmail.com.com>
2022-02-24 20:24:48 +0530

    Initial commit

--- main.py
+++ main.py
@@ -1378,7 +1378,7 @@ class TeacherView(tk.Frame):
         tk.Frame.__init__(self, master)
         self.sty = ttk.Style()
         self.sty.configure('Treeview', rowheight=46)
-        self.sty.map('Treeview', background=[('selected', '#3B3B3B') , ('focus','#212121')])
+        self.sty.map('Treeview', background=[('selected', '#3B3B3B'), ('focus', '#212121')])
         self.sty.configure("Vertical.TScrollbar",
                            background="#000000", darkcolor="#000000", lightcolor="#000000",
                            troughcolor="gray", bordercolor="gray", arrowcolor="white")
@@ -1401,7 +1401,6 @@ class TeacherView(tk.Frame):
         # self.teacher_recodes.heading("Gender", text="Gender")
         # self.teacher_recodes.heading("Subjects", text="Subjects")
 
-
         self.teacher_recodes.column("ID", width=50)
         self.teacher_recodes.column("First name", width=150)
         self.teacher_recodes.column("Last name", width=130)
@@ -1422,13 +1421,13 @@ class TeacherView(tk.Frame):
 
         count = 0
         self.teacher_recodes.tag_configure("oneColor", background="#121212",
-                                                 font='{Poppins} 8 {bold}',
-                                                foreground="#c2c2c2")
-        
+                                           font='{Poppins} 8 {bold}',
+                                           foreground="#c2c2c2")
+
         self.teacher_recodes.tag_configure("secondColor", background="#0a0a0a",
-                                                 font='{Poppins} 8 {bold}',
-                                                foreground="#c2c2c2")
-                                                
+                                           font='{Poppins} 8 {bold}',
+                                           foreground="#c2c2c2")
+
         for record in self.recodes:
             if count % 2 == 0:
                 self.teacher_recodes.insert(parent="", index='end', iid=count, values=(
@@ -1445,7 +1444,7 @@ class TeacherView(tk.Frame):
 
         # self.teacher_recodes.insert()
 
-        self.teacher_recodes.place(anchor='nw', x='0', y='146', relx="-0.24" ,bordermode='ignore')
+        self.teacher_recodes.place(anchor='nw', x='0', y='146', relx="-0.24", bordermode='ignore')
         self.teacher_recodes_scroll.configure(command=self.teacher_recodes.yview)
         self.teacher_recodes_scroll.configure(orient='vertical', )
         self.teacher_recodes_scroll.place(anchor='nw', height='44', x='0', y='150')
@@ -1465,7 +1464,8 @@ class TeacherView(tk.Frame):
                                                         relief="flat",
                                                         overrelief="flat")
         self.teacher_view_theme_change_button.configure(image=self.img_DarkThemeimg,
-                                                        text='button2')
+                                                        text='button2',
+                                                        command=self.changeTeacherViewTheme)
         self.teacher_view_theme_change_button.place(anchor='nw',
                                                     x='60',
                                                     y='10')
@@ -1493,104 +1493,99 @@ class TeacherView(tk.Frame):
             messagebox.showerror("File Missing", "img_img4.png is missing")
         self.Teacher_view_close_button = tk.Button(self)
         self.Teacher_view_close_button.configure(activebackground='#121212',
-                                                        activeforeground='#121212',
-                                                        background='#121212',
-                                                        borderwidth='0')
+                                                 activeforeground='#121212',
+                                                 background='#121212',
+                                                 borderwidth='0')
         self.Teacher_view_close_button.configure(cursor='hand2',
-                                                        foreground='#121212',
-                                                        highlightbackground='#121212',
-                                                        highlightcolor='#121212')
+                                                 foreground='#121212',
+                                                 highlightbackground='#121212',
+                                                 highlightcolor='#121212')
         self.Teacher_view_close_button.configure(highlightthickness='1',
-                                                        image=self.img_img4,
-                                                        relief='flat',
-                                                        command=self.master.on_close)
+                                                 image=self.img_img4,
+                                                 relief='flat',
+                                                 command=self.master.on_close)
         self.Teacher_view_close_button.place(anchor='nw',
-                                                    x='760',
-                                                    y='8')
+                                             x='760',
+                                             y='8')
         self.View_Teacher_label = tk.Label(self)
         self.View_Teacher_label.configure(background='#121212',
-                                              borderwidth='0',
-                                              font='{Poppins} 28 {bold}',
-                                              foreground='#ffffff')
+                                          borderwidth='0',
+                                          font='{Poppins} 28 {bold}',
+                                          foreground='#ffffff')
         self.View_Teacher_label.configure(text='View Teacher')
         self.View_Teacher_label.place(anchor='nw',
-                                          x='274',
-                                          y='28')
+                                      x='274',
+                                      y='28')
 
         self.Teacher_ID_label = tk.Label(self)
         self.Teacher_ID_label.configure(background='#121212',
-                                              borderwidth='0',
-                                              font='{Poppins} 10 {bold}',
-                                              foreground='#ffffff')
+                                        borderwidth='0',
+                                        font='{Poppins} 10 {bold}',
+                                        foreground='#ffffff')
         self.Teacher_ID_label.configure(text='ID')
         self.Teacher_ID_label.place(anchor='nw',
-                                          x='16',
-                                          y='115')
+                                    x='16',
+                                    y='115')
 
         self.Teacher_First_Name_label = tk.Label(self)
         self.Teacher_First_Name_label.configure(background='#121212',
-                                              borderwidth='0',
-                                              font='{Poppins} 10 {bold}',
-                                              foreground='#ffffff')
+                                                borderwidth='0',
+                                                font='{Poppins} 10 {bold}',
+                                                foreground='#ffffff')
         self.Teacher_First_Name_label.configure(text='First Name')
         self.Teacher_First_Name_label.place(anchor='nw',
-                                          x='55',
-                                          y='115')
-
+                                            x='55',
+                                            y='115')
 
         self.Teacher_Second_Name_label = tk.Label(self)
         self.Teacher_Second_Name_label.configure(background='#121212',
-                                              borderwidth='0',
-                                              font='{Poppins} 10 {bold}',
-                                              foreground='#ffffff')
+                                                 borderwidth='0',
+                                                 font='{Poppins} 10 {bold}',
+                                                 foreground='#ffffff')
         self.Teacher_Second_Name_label.configure(text='Second Name')
         self.Teacher_Second_Name_label.place(anchor='nw',
-                                          x='180',
-                                          y='115')
-
+                                             x='180',
+                                             y='115')
 
         self.Teacher_Age_label = tk.Label(self)
         self.Teacher_Age_label.configure(background='#121212',
-                                              borderwidth='0',
-                                              font='{Poppins} 10 {bold}',
-                                              foreground='#ffffff')
+                                         borderwidth='0',
+                                         font='{Poppins} 10 {bold}',
+                                         foreground='#ffffff')
         self.Teacher_Age_label.configure(text='Age')
         self.Teacher_Age_label.place(anchor='nw',
-                                          x='340',
-                                          y='115')
+                                     x='340',
+                                     y='115')
 
-        
         self.Teacher_Phone_Number_label = tk.Label(self)
         self.Teacher_Phone_Number_label.configure(background='#121212',
-                                              borderwidth='0',
-                                              font='{Poppins} 10 {bold}',
-                                              foreground='#ffffff')
+                                                  borderwidth='0',
+                                                  font='{Poppins} 10 {bold}',
+                                                  foreground='#ffffff')
         self.Teacher_Phone_Number_label.configure(text='Phone Number')
         self.Teacher_Phone_Number_label.place(anchor='nw',
-                                          x='390',
-                                          y='115')
-
+                                              x='390',
+                                              y='115')
 
         self.Teacher_Gender_label = tk.Label(self)
         self.Teacher_Gender_label.configure(background='#121212',
-                                              borderwidth='0',
-                                              font='{Poppins} 10 {bold}',
-                                              foreground='#ffffff')
+                                            borderwidth='0',
+                                            font='{Poppins} 10 {bold}',
+                                            foreground='#ffffff')
         self.Teacher_Gender_label.configure(text='Gender')
         self.Teacher_Gender_label.place(anchor='nw',
-                                          x='520',
-                                          y='115')
-        
+                                        x='520',
+                                        y='115')
+
         self.Teacher_Main_Subject_label = tk.Label(self)
         self.Teacher_Main_Subject_label.configure(background='#121212',
-                                              borderwidth='0',
-                                              font='{Poppins} 10 {bold}',
-                                              foreground='#ffffff')
+                                                  borderwidth='0',
+                                                  font='{Poppins} 10 {bold}',
+                                                  foreground='#ffffff')
         self.Teacher_Main_Subject_label.configure(text='Main Subject')
         self.Teacher_Main_Subject_label.place(anchor='nw',
-                                          x='600',
-                                          y='115')
-
+                                              x='600',
+                                              y='115')
 
         self.configure(background='#121212',
                        height='515',
@@ -1600,9 +1595,13 @@ class TeacherView(tk.Frame):
                    x='0',
                    y='0')
 
-
     def goBackToTeacherHome(self):
         self.master.switch_frame(TeacherHome)
+
+    def changeTeacherViewTheme(self):
+        print("Work")
+        if self["background"] == "#121212":
+            pass
 
 
 # run_the_app_in_Hr_____VVVVVVVVVVV@
